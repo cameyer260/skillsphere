de1009faeca9fbeb5c14bdfb8b0436cc42138d04
Christopher Meyer <cameyer06@gmail.com>
2024-12-31 13:54:00 -0600

    some bigger changes to the layout of the website. home page layout getting more put together, and some default styling in the project that was causing issues was found and removed. still using temporary fake data on web pages while i design them and put the front end together

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,20 +33,20 @@ export default function RootLayout({
 					enableSystem
 					disableTransitionOnChange
 				>
-					<main className="min-h-screen flex flex-col items-center">
-						<div className="flex-1 w-full flex flex-col gap-20 items-center">
-							<nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
+					<main>
+						<div>
+							<nav className="w-full flex justify-center border-b border-b-foreground/30 h-16">
 								<div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
 									<div className="flex gap-5 items-center font-semibold">
 										<Link href={"/"}>Home</Link>
 										<div className="flex items-center gap-2">
 											<ThemeSwitcher />
-										</div>
+										</div>  
 									</div>
 									<HeaderAuth />
 								</div>
 							</nav>
-							<div className="flex flex-col gap-20 max-w-5xl p-5">
+							<div>
 								{children}
 							</div>
 						</div>
--- app/protected/home/page.tsx
+++ app/protected/home/page.tsx
@@ -1,20 +1,276 @@
+import Link from "next/link";
+
 export default function Home() {
 	return (
-		<div className="w-screen bg-white m-0">
-			<div className="flex w-full h-screen">
-				<div className="w-1/3 bg-red">
-					<h1>Games</h1>
-				</div>
-				<div className="w-1/3 bg-green">
-					<h1>Friends</h1>
-				</div>
-				<div className="w-1/3 bg-blue">
-					<h1>Leaderboard</h1>
-				</div>
+		<div className="flex flex-row w-full">
+			<div className="w-3/12 border-r border-r-foreground/30 flex flex-col text-xl px-2 [&>a]:border-b [&>a]:border-b-foreground/10 [&>a]:py-2">
+				<h1 className="text-3xl border-b border-b-foreground/30 py-2">
+					Friends
+				</h1>
+				<Link href={"/DNE"}>catalina</Link>
+				<Link href={"/DNE"}>christopher</Link>
+				<Link href={"/DNE"}>leon</Link>
+				<Link href={"/DNE"}>paola</Link>
+				<Link href={"/DNE"}>rylee</Link>
+				<Link href={"/DNE"}>catalina</Link>
+				<Link href={"/DNE"}>christopher</Link>
+				<Link href={"/DNE"}>leon</Link>
+				<Link href={"/DNE"}>paola</Link>
+				<Link href={"/DNE"}>rylee</Link>
+				<Link href={"/DNE"}>catalina</Link>
+				<Link href={"/DNE"}>christopher</Link>
+				<Link href={"/DNE"}>leon</Link>
+				<Link href={"/DNE"}>paola</Link>
+				<Link href={"/DNE"}>rylee</Link>
+				<Link href={"/DNE"}>catalina</Link>
+				<Link href={"/DNE"}>christopher</Link>
+				<Link href={"/DNE"}>leon</Link>
+				<Link href={"/DNE"}>paola</Link>
+				<Link href={"/DNE"}>rylee</Link>
+				<Link href={"/DNE"}>catalina</Link>
+				<Link href={"/DNE"}>christopher</Link>
+				<Link href={"/DNE"}>leon</Link>
+				<Link href={"/DNE"}>paola</Link>
+				<Link href={"/DNE"}>rylee</Link>
+				<Link href={"/DNE"}>catalina</Link>
+				<Link href={"/DNE"}>christopher</Link>
+				<Link href={"/DNE"}>leon</Link>
+				<Link href={"/DNE"}>paola</Link>
+				<Link href={"/DNE"}>rylee</Link>
+				<Link href={"/DNE"}>catalina</Link>
+				<Link href={"/DNE"}>christopher</Link>
+				<Link href={"/DNE"}>leon</Link>
+				<Link href={"/DNE"}>paola</Link>
+				<Link href={"/DNE"}>rylee</Link>
+				<Link href={"/DNE"}>catalina</Link>
+				<Link href={"/DNE"}>christopher</Link>
+				<Link href={"/DNE"}>leon</Link>
+				<Link href={"/DNE"}>paola</Link>
+				<Link href={"/DNE"}>rylee</Link>
+				<Link href={"/DNE"}>catalina</Link>
+				<Link href={"/DNE"}>christopher</Link>
+				<Link href={"/DNE"}>leon</Link>
+				<Link href={"/DNE"}>paola</Link>
+				<Link href={"/DNE"}>rylee</Link>
 			</div>
-			<div className="p-4">
-				<h2>More Content Below</h2>
-				<p>Your extra text or sections go here...</p>
+			<div className="w-9/12 px-2 text-xl">
+				<h1 className="text-3xl border-b border-b-foreground/30 py-2">
+					Top Games
+				</h1>
+				<div className="m-2 grid grid-cols-4 gap-4 p-4 [&>div]:text-center [&>div]:border [&>div]:rounded-lg [&>div]:border-current">
+					<div>
+                        1
+                    </div>
+                    <div>
+                        2
+                    </div>
+                    <div>
+                        3
+                    </div>
+                    <div>
+                        4
+                    </div>
+                    <div>
+                        1
+                    </div>
+                    <div>
+                        2
+                    </div>
+                    <div>
+                        3
+                    </div>
+                    <div>
+                        4
+                    </div>
+                    <div>
+                        1
+                    </div>
+                    <div>
+                        2
+                    </div>
+                    <div>
+                        3
+                    </div>
+                    <div>
+                        4
+                    </div><div>
+                        1
+                    </div>
+                    <div>
+                        2
+                    </div>
+                    <div>
+                        3
+                    </div>
+                    <div>
+                        4
+                    </div><div>
+                        1
+                    </div>
+                    <div>
+                        2
+                    </div>
+                    <div>
+                        3
+                    </div>
+                    <div>
+                        4
+                    </div><div>
+                        1
+                    </div>
+                    <div>
+                        2
+                    </div>
+                    <div>
+                        3
+                    </div>
+                    <div>
+                        4
+                    </div><div>
+                        1
+                    </div>
+                    <div>
+                        2
+                    </div>
+                    <div>
+                        3
+                    </div>
+                    <div>
+                        4
+                    </div><div>
+                        1
+                    </div>
+                    <div>
+                        2
+                    </div>
+                    <div>
+                        3
+                    </div>
+                    <div>
+                        4
+                    </div><div>
+                        1
+                    </div>
+                    <div>
+                        2
+                    </div>
+                    <div>
+                        3
+                    </div>
+                    <div>
+                        4
+                    </div><div>
+                        1
+                    </div>
+                    <div>
+                        2
+                    </div>
+                    <div>
+                        3
+                    </div>
+                    <div>
+                        4
+                    </div><div>
+                        1
+                    </div>
+                    <div>
+                        2
+                    </div>
+                    <div>
+                        3
+                    </div>
+                    <div>
+                        4
+                    </div><div>
+                        1
+                    </div>
+                    <div>
+                        2
+                    </div>
+                    <div>
+                        3
+                    </div>
+                    <div>
+                        4
+                    </div><div>
+                        1
+                    </div>
+                    <div>
+                        2
+                    </div>
+                    <div>
+                        3
+                    </div>
+                    <div>
+                        4
+                    </div><div>
+                        1
+                    </div>
+                    <div>
+                        2
+                    </div>
+                    <div>
+                        3
+                    </div>
+                    <div>
+                        4
+                    </div><div>
+                        1
+                    </div>
+                    <div>
+                        2
+                    </div>
+                    <div>
+                        3
+                    </div>
+                    <div>
+                        4
+                    </div><div>
+                        1
+                    </div>
+                    <div>
+                        2
+                    </div>
+                    <div>
+                        3
+                    </div>
+                    <div>
+                        4
+                    </div><div>
+                        1
+                    </div>
+                    <div>
+                        2
+                    </div>
+                    <div>
+                        3
+                    </div>
+                    <div>
+                        4
+                    </div><div>
+                        1
+                    </div>
+                    <div>
+                        2
+                    </div>
+                    <div>
+                        3
+                    </div>
+                    <div>
+                        4
+                    </div><div>
+                        1
+                    </div>
+                    <div>
+                        2
+                    </div>
+                    <div>
+                        3
+                    </div>
+                    <div>
+                        4
+                    </div>
+				</div>
 			</div>
 		</div>
 	);
