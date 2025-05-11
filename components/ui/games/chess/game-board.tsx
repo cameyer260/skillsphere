8b62927e7940a96badb317f805871030c9e5facc
Christopher Meyer <cameyer06@gmail.com>
2025-05-10 22:51:13 -0500

    board correctly renders with all pieces on the board. pawn moves correctly, now to work on all other pieces move logic

--- components/ui/games/chess/game-board.tsx
+++ components/ui/games/chess/game-board.tsx
@@ -2,14 +2,24 @@
 
 import { useState, useEffect } from "react";
 import Image from "next/image";
-import { Pawn, Rook, Knight, Bishop, Queen, King, Piece, PieceImage, Color } from "./piece";
+import { Pawn, Rook, Knight, Bishop, Queen, King, Piece, PieceImage } from "./piece";
 
-type PlayerColor = "white" | "black";
+export type PlayerColor = "white" | "black";
 
-class Board {
+export interface Position {
+    r: number,
+    c: number,
+}
+
+export class Board {
   boardMatrix: (Piece | null)[][];
 
-  constructor(pc: PlayerColor) {
+  constructor(pc: PlayerColor, bM?: (Piece | null)[][]) {
+    if (bM) {
+      this.boardMatrix = bM;
+      return;
+    }
+
     let temp = [
       (pc === "white" ? ['r', 'k', 'b', 'q', 'K', 'b', 'k', 'r'] : ['r', 'k', 'b', 'K', 'q', 'b', 'k', 'r'] ),
       ['p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'],
@@ -43,11 +53,71 @@ class Board {
       });
     });
   };
+};
+
+const handleMove = (
+  position: Position, 
+  board: Board, 
+  playerColor: PlayerColor, 
+  turn: PlayerColor, 
+  clickedPiece: Position | null, 
+  setClickedPiece: React.Dispatch<React.SetStateAction<Position | null>>, 
+  setTurn: React.Dispatch<React.SetStateAction<PlayerColor>>, 
+  setBoard: React.Dispatch<React.SetStateAction<Board>>,
+  setMoveError: React.Dispatch<React.SetStateAction<Position | null>>
+) => {
+  /**
+   * 1. call move on piece, will return true on success, false on fail
+   * 2. if fail, highlight toPos red temporarily
+   * 3. update all states (except board because that is handled in piece's move function): turn, clickedPiece
+  **/
+  if (!board || !clickedPiece) return;
+  if (!board.boardMatrix[clickedPiece.r][clickedPiece.c]?.move(clickedPiece, position, board, setBoard)) {
+    // move is called and if it fails we highlight toPos in this block
+    setMoveError(position);
+    setTimeout(() => {
+      setMoveError(null);
+    }, 1000);
+    setClickedPiece(null);
+
+    return; // exit function without updating turn because on failure to move turn does not change
+  }
+
+  // update states assuming success
+  // setTurn(playerColor === "white" ? "black" : "white");          TEMP DISABLE FOR DEVELOPMENT
+  setClickedPiece(null);
 }
+
+const handleClick = (
+  position: Position, 
+  board: Board, 
+  playerColor: PlayerColor, 
+  turn: PlayerColor, 
+  clickedPiece: Position | null, 
+  setClickedPiece: React.Dispatch<React.SetStateAction<Position | null>>, 
+  setTurn: React.Dispatch<React.SetStateAction<PlayerColor>>, 
+  setBoard: React.Dispatch<React.SetStateAction<Board>>, 
+  setMoveError: React.Dispatch<React.SetStateAction<Position | null>>
+) => {
+  /**
+   * 1. check if board is null, if it is exit this function
+   * 2. check that it is our players turn, if it is not exit this function
+   * 3. check if the square is not null and is our piece, if not exit this function
+   * 4. check if clicked piece is null. if it is, set it, else, call handlemove function
+  **/ 
+  if (!board) return;
+  if (playerColor !== turn) return;
+  if (!clickedPiece && board.boardMatrix[position.r][position.c]?.color !== playerColor) return;
+  if (!clickedPiece) {
+    setClickedPiece(position);
+    return;
+  }
+  handleMove(position, board, playerColor, turn, clickedPiece, setClickedPiece, setTurn, setBoard, setMoveError);
+};
 
 export default function gameBoard() {
   // current players turn
-  const [ turn, setTurn ] = useState<"white" | "black">("white"); // white always starts first in chess
+  const [ turn, setTurn ] = useState<PlayerColor>("white"); // white always starts first in chess
 
   // our players color
   const [ playerColor, setPlayerColor ] = useState<PlayerColor>(() => {
@@ -55,15 +125,20 @@ export default function gameBoard() {
     return (rand === 0 ? "white" : "black");
   });
 
-  const [ board, setBoard ] = useState<Board>(new Board(playerColor));
+  const [ board, setBoard ] = useState<Board | null>(null);
+
+  // for our players current clicked piece, after it is set (when the player clicks a piece), they can move the piece by clicking a valid square
+  const [ clickedPiece, setClickedPiece ] = useState<Position | null>(null);
+
+  const [ moveError, setMoveError ] = useState<Position | null>(null);
 
   useEffect(() => {
-    
-  }, []);
+    setBoard(new Board(playerColor));
+  }, [playerColor]);
 
   return (
     <div className="flex flex-col items-center justify-center w-5/12 aspect-square max-h-[calc(100%-2rem)]">
-      {board.boardMatrix.map((arr, r) => (
+      {board?.boardMatrix.map((arr, r) => (
         <div key={r} className="flex flex-row w-full flex-1">
           {arr.map((el, c) => (
             <div 
@@ -73,8 +148,17 @@ export default function gameBoard() {
                 (c === 0 || c % 2 ===0 ? "rgb(235, 236, 208)" : "rgb(115, 149, 82)") :
                 (c === 0 || c % 2 ===0 ? "rgb(115, 149, 82)" : "rgb(235, 236, 208)")}
               `}}
+              onClick={() => handleClick({r: r, c: c}, board, playerColor, turn, clickedPiece, setClickedPiece, setTurn, setBoard, setMoveError)}
             >
-              {el && <PieceImage type={el.type} color={el.color} />}
+              {el && (
+                <div className="absolute inset-0 pointer-events-none">
+                  <PieceImage type={el.type} color={el.color} />
+                </div>
+              )}
+              {((moveError?.r === r && moveError?.c === c) || (clickedPiece?.r === r && clickedPiece?.c === c)) && (
+                <div className="absolute inset-0 pointer-events-none bg-red-500/20">
+                </div>
+              )}
             </div>
           ))}
         </div>
--- components/ui/games/chess/piece.tsx
+++ components/ui/games/chess/piece.tsx
@@ -1,78 +1,167 @@
 import Image from "next/image";
 import react from "react";
+import type { PlayerColor } from "./game-board";
+import { Position, Board } from "./game-board";
 
 type PieceType = "Pawn" | "Rook" | "Knight" | "Bishop" | "Queen" | "King";
-export type Color = "white" | "black";
-
 export interface Piece {
   type: PieceType;
-  // color and position are instantiated when the game starts 
-  color: Color;
+  color: PlayerColor;
+  updateBoard: (currentPos: Position, toPos: Position, board: Board, setBoard: React.Dispatch<React.SetStateAction<Board>>) => void;
+  move: (currentPos: Position, toPos: Position, board: Board, setBoard: React.Dispatch<React.SetStateAction<Board>>) => boolean; // returns success or fail
 };
 
 export class Pawn implements Piece {
   type: PieceType;
-  color: Color;
+  color: PlayerColor;
 
-  constructor(col: Color) {
+  constructor(col: PlayerColor) {
     this.type = "Pawn";
     this.color = col;
   };
+
+  updateBoard(currentPos: Position, toPos: Position, board: Board, setBoard: React.Dispatch<React.SetStateAction<Board>>) {
+    const temp = board.boardMatrix.map(row => [...row]);
+    temp[toPos.r][toPos.c] = temp[currentPos.r][currentPos.c];
+    temp[currentPos.r][currentPos.c] = null;
+    setBoard(new Board(this.color, temp));
+  }
+
+  move(currentPos: Position, toPos: Position, board: Board, setBoard: React.Dispatch<React.SetStateAction<Board>>) {
+    console.log(currentPos);
+    console.log(toPos);
+    // CASE 1: pawn is moving into open spot forward (1 or 2 spaces forward)
+    if (currentPos.c === toPos.c) {
+      // moving forward 1 space into open spot
+      if (currentPos.r === toPos.r+1 && !board.boardMatrix[toPos.r][toPos.c]) {
+        this.updateBoard(currentPos, toPos, board, setBoard);
+        return true;
+      }
+      // moving forward 2 spaces into open spot
+      if (currentPos.r === toPos.r+2 && !board.boardMatrix[toPos.r][toPos.c] && !board.boardMatrix[toPos.r+1][toPos.c] && currentPos.r === 6) {
+        this.updateBoard(currentPos, toPos, board, setBoard);
+        return true;
+      }
+    }
+
+    // CASE 2: diagonal move  --------------------------------------------------------------------------------------------------------STILL NEEDS TESTING
+    if ((currentPos.c === toPos.c-1 || currentPos.c === toPos.c+1) && (currentPos.r === toPos.r+1) && (board.boardMatrix[toPos.r][toPos.c] && board.boardMatrix[toPos.r][toPos.c].color !== this.color)) {
+      console.log("moving diagonal");
+      this.updateBoard(currentPos, toPos, board, setBoard);
+      return true;
+    }
+    return false;
+  }
 }
 
 export class Rook implements Piece {
   type: PieceType;
-  color: Color;
+  color: PlayerColor;
 
-  constructor(col: Color) {
+  constructor(col: PlayerColor) {
     this.type = "Rook";
     this.color = col;
   };
+
+  updateBoard(currentPos: Position, toPos: Position, board: Board, setBoard: React.Dispatch<React.SetStateAction<Board>>) {
+    const temp = board.map(row => [...row]);
+    temp[toPos.r][toPos.c] = temp[currentPos.r][currentPos.c];
+    temp[currentPos.r][currentPos.c] = null;
+    setBoard(temp);
+  }
+
+  move(currentPos: Position, toPos: Position, board: Board, setBoard: React.Dispatch<React.SetStateAction<Board>>) {
+    
+  }
 }
 
 export class Knight implements Piece {
   type: PieceType;
-  color: Color;
+  color: PlayerColor;
   
-  constructor(col: Color) {
+  constructor(col: PlayerColor) {
     this.type = "Knight";
     this.color = col;
   };
+
+  updateBoard(currentPos: Position, toPos: Position, board: Board, setBoard: React.Dispatch<React.SetStateAction<Board>>) {
+    const temp = board.map(row => [...row]);
+    temp[toPos.r][toPos.c] = temp[currentPos.r][currentPos.c];
+    temp[currentPos.r][currentPos.c] = null;
+    setBoard(temp);
+  }
+
+  move(currentPos: Position, toPos: Position, board: Board, setBoard: React.Dispatch<React.SetStateAction<Board>>) {
+
+  }
 }
 
 export class Bishop implements Piece {
   type: PieceType;
-  color: Color;
+  color: PlayerColor;
   
-  constructor(col: Color) {
+  constructor(col: PlayerColor) {
     this.type = "Bishop";
     this.color = col;
   };
+
+  updateBoard(currentPos: Position, toPos: Position, board: Board, setBoard: React.Dispatch<React.SetStateAction<Board>>) {
+    const temp = board.map(row => [...row]);
+    temp[toPos.r][toPos.c] = temp[currentPos.r][currentPos.c];
+    temp[currentPos.r][currentPos.c] = null;
+    setBoard(temp);
+  }
+
+  move(currentPos: Position, toPos: Position, board: Board, setBoard: React.Dispatch<React.SetStateAction<Board>>) {
+
+  }
 }
 
 export class Queen implements Piece {
   type: PieceType;
-  color: Color;
+  color: PlayerColor;
   
-  constructor(col: Color) {
+  constructor(col: PlayerColor) {
     this.type = "Queen";
     this.color = col;
   };
+
+  updateBoard(currentPos: Position, toPos: Position, board: Board, setBoard: React.Dispatch<React.SetStateAction<Board>>) {
+    const temp = board.map(row => [...row]);
+    temp[toPos.r][toPos.c] = temp[currentPos.r][currentPos.c];
+    temp[currentPos.r][currentPos.c] = null;
+    setBoard(temp);
+  }
+
+  move(currentPos: Position, toPos: Position, board: Board, setBoard: React.Dispatch<React.SetStateAction<Board>>) {
+
+  }
 }
 
 export class King implements Piece {
   type: PieceType;
-  color: Color;
+  color: PlayerColor;
   
-  constructor(col: Color) {
+  constructor(col: PlayerColor) {
     this.type = "King";
     this.color = col;
   };
+
+  updateBoard(currentPos: Position, toPos: Position, board: Board, setBoard: React.Dispatch<React.SetStateAction<Board>>) {
+    const temp = board.map(row => [...row]);
+    temp[toPos.r][toPos.c] = temp[currentPos.r][currentPos.c];
+    temp[currentPos.r][currentPos.c] = null;
+    setBoard(temp);
+  }
+
+  move(currentPos: Position, toPos: Position, board: Board, setBoard: React.Dispatch<React.SetStateAction<Board>>) {
+
+  }
 }
 
 interface PieceImageProps {
   type: PieceType,
-  color: Color,
+  color: PlayerColor,
 }
 
 const typeToPieceLetter: Record<PieceType, String> = {
@@ -84,13 +173,15 @@ const typeToPieceLetter: Record<PieceType, String> = {
   "King": "k",
 }
 
-export const PieceImage: React.FC<PieceImageProps> = ({ type, color }) => {
+export const PieceImage: React.FC<PieceImageProps> = ({ type, color}) => {
   return (
-    <Image
-      src={`/game-icons/chess/${color === "white" ? "w" : "b"}${typeToPieceLetter[type]}.png`}
-      alt="bp"
-      fill 
-      className="object-contain"
-    />
+    <div className="">
+      <Image
+        src={`/game-icons/chess/${color === "white" ? "w" : "b"}${typeToPieceLetter[type]}.png`}
+        alt="bp"
+        fill 
+        className="object-contain"
+      />
+    </div>
   );
 }
