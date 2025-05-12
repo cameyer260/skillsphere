878248ee6afb003a8fd7a134d0bfa0ab88eeda25
Christopher Meyer <cameyer06@gmail.com>
2025-05-11 23:55:04 -0500

    good work done today. got typescript lsp working first which has helped tremendously. all pieces now move correctly, and the logic for castling has also been added. whats left is game logic, turns, points, bots to play against, all that. will start that tomorrow.

--- components/ui/games/chess/game-board.tsx
+++ components/ui/games/chess/game-board.tsx
@@ -1,14 +1,13 @@
 "use client"
 
 import { useState, useEffect } from "react";
-import Image from "next/image";
 import { Pawn, Rook, Knight, Bishop, Queen, King, Piece, PieceImage } from "./piece";
 
 export type PlayerColor = "white" | "black";
 
 export interface Position {
-    r: number,
-    c: number,
+  r: number,
+  c: number,
 }
 
 export class Board {
@@ -21,20 +20,20 @@ export class Board {
     }
 
     let temp = [
-      (pc === "white" ? ['r', 'k', 'b', 'q', 'K', 'b', 'k', 'r'] : ['r', 'k', 'b', 'K', 'q', 'b', 'k', 'r'] ),
+      (pc === "white" ? ['r', 'k', 'b', 'q', 'K', 'b', 'k', 'r'] : ['r', 'k', 'b', 'K', 'q', 'b', 'k', 'r']),
       ['p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'],
       ['x', 'x', 'x', 'x', 'x', 'x', 'x', 'x'],
       ['x', 'x', 'x', 'x', 'x', 'x', 'x', 'x'],
       ['x', 'x', 'x', 'x', 'x', 'x', 'x', 'x'],
       ['x', 'x', 'x', 'x', 'x', 'x', 'x', 'x'],
       ['p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'],
-      (pc === "white" ? ['r', 'k', 'b', 'q', 'K', 'b', 'k', 'r'] : ['r', 'k', 'b', 'K', 'q', 'b', 'k', 'r'] ),
+      (pc === "white" ? ['r', 'k', 'b', 'q', 'K', 'b', 'k', 'r'] : ['r', 'k', 'b', 'K', 'q', 'b', 'k', 'r']),
     ];
 
     this.boardMatrix = temp.map((arr, row) => {
       const color = (pc === "white" ? (row < 2 ? "black" : "white") : (row < 2 ? "white" : "black"));
-      return arr.map((el, col) => {
-        switch(el) {
+      return arr.map((el) => {
+        switch (el) {
           case 'p':
             return new Pawn(color);
           case 'r':
@@ -56,13 +55,12 @@ export class Board {
 };
 
 const handleMove = (
-  position: Position, 
-  board: Board, 
-  playerColor: PlayerColor, 
-  turn: PlayerColor, 
-  clickedPiece: Position | null, 
-  setClickedPiece: React.Dispatch<React.SetStateAction<Position | null>>, 
-  setTurn: React.Dispatch<React.SetStateAction<PlayerColor>>, 
+  position: Position,
+  board: Board,
+  turn: PlayerColor,
+  clickedPiece: Position | null,
+  setClickedPiece: React.Dispatch<React.SetStateAction<Position | null>>,
+  setTurn: React.Dispatch<React.SetStateAction<PlayerColor>>,
   setBoard: React.Dispatch<React.SetStateAction<Board | null>>,
   setMoveError: React.Dispatch<React.SetStateAction<Position | null>>
 ) => {
@@ -77,26 +75,26 @@ const handleMove = (
     setMoveError(position);
     setTimeout(() => {
       setMoveError(null);
-    }, 1000);
-    setClickedPiece(null);
+    }, 500);
+    setClickedPiece(null); // set clicked piece to null on success or failure to move piece
 
     return; // exit function without updating turn because on failure to move turn does not change
   }
 
   // update states assuming success
-  // setTurn(playerColor === "white" ? "black" : "white");          TEMP DISABLE FOR DEVELOPMENT
+  // setTurn(turn === "white" ? "black" : "white");          TEMP DISABLE FOR DEVELOPMENT
   setClickedPiece(null);
 }
 
 const handleClick = (
-  position: Position, 
-  board: Board, 
-  playerColor: PlayerColor, 
-  turn: PlayerColor, 
-  clickedPiece: Position | null, 
-  setClickedPiece: React.Dispatch<React.SetStateAction<Position | null>>, 
-  setTurn: React.Dispatch<React.SetStateAction<PlayerColor>>, 
-  setBoard: React.Dispatch<React.SetStateAction<Board | null>>, 
+  position: Position,
+  board: Board,
+  playerColor: PlayerColor,
+  turn: PlayerColor,
+  clickedPiece: Position | null,
+  setClickedPiece: React.Dispatch<React.SetStateAction<Position | null>>,
+  setTurn: React.Dispatch<React.SetStateAction<PlayerColor>>,
+  setBoard: React.Dispatch<React.SetStateAction<Board | null>>,
   setMoveError: React.Dispatch<React.SetStateAction<Position | null>>
 ) => {
   /**
@@ -104,7 +102,7 @@ const handleClick = (
    * 2. check that it is our players turn, if it is not exit this function
    * 3. check if the square is not null and is our piece, if not exit this function
    * 4. check if clicked piece is null. if it is, set it, else, call handlemove function
-  **/ 
+  **/
   if (!board) return;
   if (playerColor !== turn) return;
   if (!clickedPiece && board.boardMatrix[position.r][position.c]?.color !== playerColor) return;
@@ -112,25 +110,27 @@ const handleClick = (
     setClickedPiece(position);
     return;
   }
-  handleMove(position, board, playerColor, turn, clickedPiece, setClickedPiece, setTurn, setBoard, setMoveError);
+  handleMove(position, board, turn, clickedPiece, setClickedPiece, setTurn, setBoard, setMoveError);
 };
 
 export default function gameBoard() {
   // current players turn
-  const [ turn, setTurn ] = useState<PlayerColor>("white"); // white always starts first in chess
+  const [turn, setTurn] = useState<PlayerColor>("white"); // white always starts first in chess
 
   // our players color
-  const [ playerColor, setPlayerColor ] = useState<PlayerColor>(() => {
-    const rand = Math.floor(Math.random() * 2); // generate either 0 or 1, 50/50 chance
-    return (rand === 0 ? "white" : "black");
-  });
+  // const [playerColor, setPlayerColor] = useState<PlayerColor>(() => {
+  //   const rand = Math.floor(Math.random() * 2); // generate either 0 or 1, 50/50 chance
+  //   return (rand === 0 ? "white" : "black");
+  // });
+  //
+  const [playerColor, setPlayerColor] = useState<PlayerColor>("white"); // for testing and development, we always are white so we can always move pieces
 
-  const [ board, setBoard ] = useState<Board | null>(null);
+  const [board, setBoard] = useState<Board | null>(null);
 
   // for our players current clicked piece, after it is set (when the player clicks a piece), they can move the piece by clicking a valid square
-  const [ clickedPiece, setClickedPiece ] = useState<Position | null>(null);
+  const [clickedPiece, setClickedPiece] = useState<Position | null>(null);
 
-  const [ moveError, setMoveError ] = useState<Position | null>(null);
+  const [moveError, setMoveError] = useState<Position | null>(null);
 
   useEffect(() => {
     setBoard(new Board(playerColor));
@@ -141,14 +141,15 @@ export default function gameBoard() {
       {board?.boardMatrix.map((arr, r) => (
         <div key={r} className="flex flex-row w-full flex-1">
           {arr.map((el, c) => (
-            <div 
-              key={c} 
-              className="flex-1 relative" 
-              style={{ backgroundColor: `${(r === 0 || r % 2 === 0) ? 
-                (c === 0 || c % 2 ===0 ? "rgb(235, 236, 208)" : "rgb(115, 149, 82)") :
-                (c === 0 || c % 2 ===0 ? "rgb(115, 149, 82)" : "rgb(235, 236, 208)")}
+            <div
+              key={c}
+              className="flex-1 relative"
+              style={{
+                backgroundColor: `${(r === 0 || r % 2 === 0) ?
+                  (c === 0 || c % 2 === 0 ? "rgb(235, 236, 208)" : "rgb(115, 149, 82)") :
+                  (c === 0 || c % 2 === 0 ? "rgb(115, 149, 82)" : "rgb(235, 236, 208)")}
               `}}
-              onClick={() => handleClick({r: r, c: c}, board, playerColor, turn, clickedPiece, setClickedPiece, setTurn, setBoard, setMoveError)}
+              onClick={() => handleClick({ r: r, c: c }, board, playerColor, turn, clickedPiece, setClickedPiece, setTurn, setBoard, setMoveError)}
             >
               {el && (
                 <div className="absolute inset-0 pointer-events-none">
--- components/ui/games/chess/piece.tsx
+++ components/ui/games/chess/piece.tsx
@@ -7,6 +7,7 @@ type PieceType = "Pawn" | "Rook" | "Knight" | "Bishop" | "Queen" | "King";
 export interface Piece {
   type: PieceType;
   color: PlayerColor;
+  hasMoved?: boolean;
   updateBoard: (currentPos: Position, toPos: Position, board: Board, setBoard: React.Dispatch<React.SetStateAction<Board | null>>) => void;
   move: (currentPos: Position, toPos: Position, board: Board, setBoard: React.Dispatch<React.SetStateAction<Board | null>>) => boolean; // returns success or fail
 };
@@ -28,25 +29,23 @@ export class Pawn implements Piece {
   }
 
   move(currentPos: Position, toPos: Position, board: Board, setBoard: React.Dispatch<React.SetStateAction<Board | null>>) {
-    console.log(currentPos);
-    console.log(toPos);
+    if (currentPos.r === toPos.r && currentPos.c === toPos.c) return false; // if they click the same square they are on return false early
     // CASE 1: pawn is moving into open spot forward (1 or 2 spaces forward)
     if (currentPos.c === toPos.c) {
       // moving forward 1 space into open spot
-      if (currentPos.r === toPos.r+1 && !board.boardMatrix[toPos.r][toPos.c]) {
+      if (currentPos.r === toPos.r + 1 && !board.boardMatrix[toPos.r][toPos.c]) {
         this.updateBoard(currentPos, toPos, board, setBoard);
         return true;
       }
       // moving forward 2 spaces into open spot
-      if (currentPos.r === toPos.r+2 && !board.boardMatrix[toPos.r][toPos.c] && !board.boardMatrix[toPos.r+1][toPos.c] && currentPos.r === 6) {
+      if (currentPos.r === toPos.r + 2 && !board.boardMatrix[toPos.r][toPos.c] && !board.boardMatrix[toPos.r + 1][toPos.c] && currentPos.r === 6) {
         this.updateBoard(currentPos, toPos, board, setBoard);
         return true;
       }
     }
 
-    // CASE 2: diagonal move  --------------------------------------------------------------------------------------------------------STILL NEEDS TESTING
-    if ((currentPos.c === toPos.c-1 || currentPos.c === toPos.c+1) && (currentPos.r === toPos.r+1) && (board.boardMatrix[toPos.r][toPos.c] && board.boardMatrix[toPos.r][toPos.c]?.color !== this.color)) {
-      console.log("moving diagonal");
+    // CASE 2: diagonal move
+    if ((currentPos.c === toPos.c - 1 || currentPos.c === toPos.c + 1) && (currentPos.r === toPos.r + 1) && (board.boardMatrix[toPos.r][toPos.c] && board.boardMatrix[toPos.r][toPos.c]?.color !== this.color)) {
       this.updateBoard(currentPos, toPos, board, setBoard);
       return true;
     }
@@ -57,10 +56,12 @@ export class Pawn implements Piece {
 export class Rook implements Piece {
   type: PieceType;
   color: PlayerColor;
+  hasMoved: boolean; // boolean in Rook and King for castling
 
   constructor(col: PlayerColor) {
     this.type = "Rook";
     this.color = col;
+    this.hasMoved = false;
   };
 
   updateBoard(currentPos: Position, toPos: Position, board: Board, setBoard: React.Dispatch<React.SetStateAction<Board | null>>) {
@@ -71,14 +72,93 @@ export class Rook implements Piece {
   }
 
   move(currentPos: Position, toPos: Position, board: Board, setBoard: React.Dispatch<React.SetStateAction<Board | null>>) {
-    return true;
+    if (currentPos.r === toPos.r && currentPos.c === toPos.c) return false; // if they click the same square they are on return false early
+    // CASE 0: castling (check before checking if they are trying to take their own piece)
+    if (board.boardMatrix[toPos.r][toPos.c]?.type === "King") {
+      if (!this.hasMoved && !board.boardMatrix[toPos.r][toPos.c]?.hasMoved) {
+        let success = true;
+        if (currentPos.c > toPos.c) { // right rook castling left
+          for (let i = currentPos.c - 1; i > toPos.c; i--) {
+            if (board.boardMatrix[currentPos.r][i]) success = false;
+          }
+          if (success) {
+            // modified updateBoard method
+            const temp = board.boardMatrix.map(row => [...row]);
+            temp[7][5] = temp[7][7];
+            temp[7][7] = null;
+            temp[7][6] = temp[7][4];
+            temp[7][4] = null;
+            setBoard(new Board(this.color, temp));
+            return true;
+          }
+        } else { // left rook castling right
+          for (let i = currentPos.c + 1; i < toPos.c; i++) {
+            if (board.boardMatrix[currentPos.r][i]) success = false;
+          }
+          if (success) {
+            // modified updateBoard method
+            const temp = board.boardMatrix.map(row => [...row]);
+            temp[7][3] = temp[7][0];
+            temp[7][0] = null;
+            temp[7][2] = temp[7][4];
+            temp[7][4] = null;
+            setBoard(new Board(this.color, temp));
+            return true;
+          }
+        }
+      }
+    }
+    if (board.boardMatrix[toPos.r][toPos.c]?.color === this.color) return false; // if they are trying to take their own piece return false early
+    // CASE 1: horizontal move
+    if (currentPos.r === toPos.r) {
+      if (toPos.c > currentPos.c) {
+        // moving right
+        for (let i = currentPos.c + 1; i < toPos.c; i++) {
+          if (board.boardMatrix[currentPos.r][i]) return false; // if there are any pieces blocking the path return false
+        }
+        // if false not returned we have reached success
+        this.updateBoard(currentPos, toPos, board, setBoard);
+        this.hasMoved = true;
+        return true;
+      } else {
+        // moving left
+        for (let i = currentPos.c - 1; i > toPos.c; i--) {
+          if (board.boardMatrix[currentPos.r][i]) return false; // if there are any pieces blocking the path return false
+        }
+        // if false not returned we have reached success
+        this.updateBoard(currentPos, toPos, board, setBoard);
+        this.hasMoved = true;
+        return true;
+      }
+    } else if (currentPos.c === toPos.c) { // CASE 2: vertical move
+      if (toPos.r > currentPos.r) {
+        // moving downward
+        for (let i = currentPos.r + 1; i < toPos.r; i++) {
+          if (board.boardMatrix[i][currentPos.c]) return false; // if there are any pieces blocking the path return false
+        }
+        // if false not returned we have reached success
+        this.updateBoard(currentPos, toPos, board, setBoard);
+        this.hasMoved = true;
+        return true;
+      } else {
+        // moving upward
+        for (let i = currentPos.r - 1; i > toPos.r; i--) {
+          if (board.boardMatrix[i][currentPos.c]) return false; // if there are any pieces blocking the path return false
+        }
+        // if false not returned we have reached success
+        this.updateBoard(currentPos, toPos, board, setBoard);
+        this.hasMoved = true;
+        return true;
+      }
+    }
+    return false;
   }
 }
 
 export class Knight implements Piece {
   type: PieceType;
   color: PlayerColor;
-  
+
   constructor(col: PlayerColor) {
     this.type = "Knight";
     this.color = col;
@@ -92,14 +172,37 @@ export class Knight implements Piece {
   }
 
   move(currentPos: Position, toPos: Position, board: Board, setBoard: React.Dispatch<React.SetStateAction<Board | null>>) {
-    return true;
+    if (currentPos.r === toPos.r && currentPos.c === toPos.c) return false; // if they click the same square they are on return false early
+    if (board.boardMatrix[toPos.r][toPos.c]?.color === this.color) return false; // if they are trying to take their own piece return false early
+
+    // CASE 1: moving up left
+    if ((toPos.r === currentPos.r - 2 && toPos.c === currentPos.c - 1) || (toPos.r === currentPos.r - 1 && toPos.c === currentPos.c - 2)) {
+      this.updateBoard(currentPos, toPos, board, setBoard);
+      return true;
+    }
+    // CASE 2: moving up right 
+    if ((toPos.r === currentPos.r - 2 && toPos.c === currentPos.c + 1) || (toPos.r === currentPos.r - 1 && toPos.c === currentPos.c + 2)) {
+      this.updateBoard(currentPos, toPos, board, setBoard);
+      return true;
+    }
+    // CASE 3: moving down left
+    if ((toPos.r === currentPos.r + 2 && toPos.c === currentPos.c - 1) || (toPos.r === currentPos.r + 1 && toPos.c === currentPos.c - 2)) {
+      this.updateBoard(currentPos, toPos, board, setBoard);
+      return true;
+    }
+    // CASE 4: moving down right
+    if ((toPos.r === currentPos.r + 2 && toPos.c === currentPos.c + 1) || (toPos.r === currentPos.r + 1 && toPos.c === currentPos.c + 2)) {
+      this.updateBoard(currentPos, toPos, board, setBoard);
+      return true;
+    }
+    return false;
   }
 }
 
 export class Bishop implements Piece {
   type: PieceType;
   color: PlayerColor;
-  
+
   constructor(col: PlayerColor) {
     this.type = "Bishop";
     this.color = col;
@@ -113,14 +216,50 @@ export class Bishop implements Piece {
   }
 
   move(currentPos: Position, toPos: Position, board: Board, setBoard: React.Dispatch<React.SetStateAction<Board | null>>) {
-    return true;
+    if (currentPos.r === toPos.r && currentPos.c === toPos.c) return false; // if they click the same square they are on return false early
+    if (board.boardMatrix[toPos.r][toPos.c]?.color === this.color) return false; // if they are trying to take their own piece return false early
+    if (Math.abs(currentPos.r - toPos.r) === Math.abs(currentPos.c - toPos.c)) { // if the absolute value of the difference of row index and column index the spots are diagonal
+      // CASE 1: move up right
+      if (toPos.r < currentPos.r && toPos.c > currentPos.c) {
+        for (let i = 1; i < Math.abs(currentPos.r - toPos.r); i++) { // i represent the # of diagonal spots traveled
+          if (board.boardMatrix[currentPos.r - i][currentPos.c + 1]) return false;
+        }
+        this.updateBoard(currentPos, toPos, board, setBoard);
+        return true;
+      }
+      // CASE 2: move up left
+      if (toPos.r < currentPos.r && toPos.c < currentPos.c) {
+        for (let i = 1; i < Math.abs(currentPos.r - toPos.r); i++) { // i represent the # of diagonal spots traveled
+          if (board.boardMatrix[currentPos.r - i][currentPos.c - 1]) return false;
+        }
+        this.updateBoard(currentPos, toPos, board, setBoard);
+        return true;
+      }
+      // CASE 3: move down right
+      if (toPos.r > currentPos.r && toPos.c > currentPos.c) {
+        for (let i = 1; i < Math.abs(currentPos.r - toPos.r); i++) { // i represent the # of diagonal spots traveled
+          if (board.boardMatrix[currentPos.r + i][currentPos.c + 1]) return false;
+        }
+        this.updateBoard(currentPos, toPos, board, setBoard);
+        return true;
+      }
+      // CASE 4: move down left
+      if (toPos.r > currentPos.r && toPos.c < currentPos.c) {
+        for (let i = 1; i < Math.abs(currentPos.r - toPos.r); i++) { // i represent the # of diagonal spots traveled
+          if (board.boardMatrix[currentPos.r + i][currentPos.c - 1]) return false;
+        }
+        this.updateBoard(currentPos, toPos, board, setBoard);
+        return true;
+      }
+    }
+    return false;
   }
 }
 
 export class Queen implements Piece {
   type: PieceType;
   color: PlayerColor;
-  
+
   constructor(col: PlayerColor) {
     this.type = "Queen";
     this.color = col;
@@ -134,17 +273,32 @@ export class Queen implements Piece {
   }
 
   move(currentPos: Position, toPos: Position, board: Board, setBoard: React.Dispatch<React.SetStateAction<Board | null>>) {
-    return true;
+    if (currentPos.r === toPos.r && currentPos.c === toPos.c) return false; // if they click the same square they are on return false early
+    if (board.boardMatrix[toPos.r][toPos.c]?.color === this.color) return false; // if they are trying to take their own piece return false early
+    // Queen is able to move like a rook and bishop, so just create temp pieces of those and if their moves move functions return true then it's a success
+    let tempRook = new Rook(this.color);
+    if (tempRook.move(currentPos, toPos, board, setBoard)) {
+      this.updateBoard(currentPos, toPos, board, setBoard);
+      return true;
+    }
+    let tempBishop = new Bishop(this.color);
+    if (tempBishop.move(currentPos, toPos, board, setBoard)) {
+      this.updateBoard(currentPos, toPos, board, setBoard);
+      return true;
+    }
+    return false;
   }
 }
 
 export class King implements Piece {
   type: PieceType;
   color: PlayerColor;
-  
+  hasMoved: boolean;
+
   constructor(col: PlayerColor) {
     this.type = "King";
     this.color = col;
+    this.hasMoved = false;
   };
 
   updateBoard(currentPos: Position, toPos: Position, board: Board, setBoard: React.Dispatch<React.SetStateAction<Board | null>>) {
@@ -155,7 +309,60 @@ export class King implements Piece {
   }
 
   move(currentPos: Position, toPos: Position, board: Board, setBoard: React.Dispatch<React.SetStateAction<Board | null>>) {
-    return true;
+    if (currentPos.r === toPos.r && currentPos.c === toPos.c) return false; // if they click the same square they are on return false early
+    // CASE 0: castling (check before checking if they are trying to take their own piece)
+    if (board.boardMatrix[toPos.r][toPos.c]?.type === "Rook") {
+      if (!this.hasMoved && !board.boardMatrix[toPos.r][toPos.c]?.hasMoved) {
+        let success = true;
+        if (currentPos.c > toPos.c) { // right rook castling left
+          for (let i = currentPos.c - 1; i > toPos.c; i--) {
+            if (board.boardMatrix[currentPos.r][i]) success = false;
+          }
+          if (success) {
+            // modified updateBoard method
+            const temp = board.boardMatrix.map(row => [...row]);
+            temp[7][3] = temp[7][0];
+            temp[7][0] = null;
+            temp[7][2] = temp[7][4];
+            temp[7][4] = null;
+            setBoard(new Board(this.color, temp));
+            return true;
+
+          }
+        } else { // left rook castling right
+          for (let i = currentPos.c + 1; i < toPos.c; i++) {
+            if (board.boardMatrix[currentPos.r][i]) success = false;
+          }
+          if (success) {
+            // modified updateBoard method
+            const temp = board.boardMatrix.map(row => [...row]);
+            temp[7][5] = temp[7][7];
+            temp[7][7] = null;
+            temp[7][6] = temp[7][4];
+            temp[7][4] = null;
+            setBoard(new Board(this.color, temp));
+            return true;
+          }
+        }
+      }
+    }
+    if (board.boardMatrix[toPos.r][toPos.c]?.color === this.color) return false; // if they are trying to take their own piece return false early
+    // check pieces brute force, go in order of counter clockwise starting at top left square
+    let success = false;
+    if (toPos.r === currentPos.r - 1 && toPos.c === currentPos.c - 1) success = true;
+    if (toPos.r === currentPos.r && toPos.c === currentPos.c - 1) success = true;
+    if (toPos.r === currentPos.r + 1 && toPos.c === currentPos.c - 1) success = true;
+    if (toPos.r === currentPos.r + 1 && toPos.c === currentPos.c) success = true;
+    if (toPos.r === currentPos.r + 1 && toPos.c === currentPos.c + 1) success = true;
+    if (toPos.r === currentPos.r && toPos.c === currentPos.c + 1) success = true;
+    if (toPos.r === currentPos.r - 1 && toPos.c === currentPos.c + 1) success = true;
+    if (toPos.r === currentPos.r - 1 && toPos.c === currentPos.c) success = true;
+    if (success === true) {
+      this.updateBoard(currentPos, toPos, board, setBoard);
+      this.hasMoved = true;
+    }
+
+    return success;
   }
 }
 
@@ -173,13 +380,13 @@ const typeToPieceLetter: Record<PieceType, String> = {
   "King": "k",
 }
 
-export const PieceImage: React.FC<PieceImageProps> = ({ type, color}) => {
+export const PieceImage: React.FC<PieceImageProps> = ({ type, color }) => {
   return (
     <div className="">
       <Image
         src={`/game-icons/chess/${color === "white" ? "w" : "b"}${typeToPieceLetter[type]}.png`}
         alt="bp"
-        fill 
+        fill
         className="object-contain"
       />
     </div>
