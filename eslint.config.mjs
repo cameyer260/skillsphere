7096d3393a00d997b83d9d3f6b5a8855b4590cb4
Christopher Meyer <cameyer06@gmail.com>
2025-12-10 09:32:57 -0600

    fixed a few eslint errors and some now specify the .env in my prod script for the ws

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,17 @@
-import next from 'eslint-config-next';
+import nextConfig from 'eslint-config-next';
 
 /** @type {import('eslint').Linter.FlatConfig[]} */
 export default [
-    {
-        files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
-        ...next,
+  {
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
+    rules: {
+      ...nextConfig.rules, // spread only the rules, not the whole object
     },
+    settings: {
+      ...nextConfig.settings, // optional, if needed
+    },
+    plugins: {
+      ...nextConfig.plugins, // optional, only if you need plugin references
+    },
+  },
 ];
